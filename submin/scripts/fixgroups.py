1290f9f252fed648e6d9a49fa0a16614e4649aae
skunkie <skunkiem@gmail.com>
2018-04-18 16:25:59 +0300

    fix logics in fixgroups.py

--- submin/scripts/fixgroups.py
+++ submin/scripts/fixgroups.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# How to run:
 # submin2-admin /var/lib/submin/ shell "execfile('/usr/lib/python2.7/site-packages/submin/scripts/fixgroups.py')"
 # submin2-admin /var/lib/submin/ shell "import sys; sys.argv = ['report']; execfile('/usr/lib/python2.7/site-packages/submin/scripts/fixgroups.py')"
 
@@ -76,75 +77,101 @@ ru_lat = {
 }
 
 not_allowed_characters = re.compile('[^a-zA-Z0-9\-\/]')
+permission_to_name = {'rw': 'rw', 'r': 'ro', '': 'no'}
+report_only = len(sys.argv) > 0 and sys.argv[0] == 'report'
 
 groups = group.list(user.FakeAdminUser())
+
+def get_correct_groupname(permission_by_group):
+	"""Returns correct groupname"""
+	permission = permission_to_name[permission_by_group['permission']]
+	path = '%s%s' % (permission_by_group['repository'],
+		permission_by_group['path'] if permission_by_group['path'] != '/' else '')
+	path = path.lower()
+
+	for key in ru_lat:
+		path = path.replace(key, ru_lat[key])
+
+	path = re.sub(not_allowed_characters, '', path)
+	path = re.sub('/', '_', path)
+	correct_groupname = '%s_%s' % (path, permission)
+
+	return correct_groupname
 
 for groupname in groups:
 	permissions_by_group = list(permissions.list_by_group(groupname))
 
 	if not permissions_by_group:
+		print "Group %s is not used" % groupname
 
-		if len(sys.argv) > 0 and sys.argv[0] == 'report':
-			print "Group %s is not used" % groupname
+		if report_only:
 			continue
 
 		group.Group(groupname).remove()
 		print "Removed unused group %s" % groupname
 		continue
 
-	for pbg in permissions_by_group:
-		permission = {'rw': 'rw', 'r': 'ro', '': 'no'}[pbg['permission']]
-		path = '%s%s' % (pbg['repository'], pbg['path'] if pbg['path'] != '/' else '')
-		path = path.lower()
+	permissions_to_groupname = []
+	match = False  # Gets True when the name of a group matches its permission
 
-		for key in ru_lat:
-			path = path.replace(key, ru_lat[key])
+	for permission_by_group in permissions_by_group:
+		correct_groupname = get_correct_groupname(permission_by_group)
+		if groupname == correct_groupname:
+			match = True
+		permissions_to_groupname.append((permission_by_group, groupname, correct_groupname))
 
-		path = re.sub(not_allowed_characters, '', path)
-		path = re.sub('/', '_', path)
+	for ptg in permissions_to_groupname:
+		permission_by_group, groupname, correct_groupname = ptg
+		last = permissions_to_groupname.index(ptg) == len(permissions_to_groupname) - 1
 
-		correct_groupname = '%s_%s' % (path, permission)
+		if groupname == correct_groupname:
+			print "Groupname %s matches its permission" % groupname
+			continue
 
-		if groupname != correct_groupname:
-			print "Groupname %s mismatches its permission, its name should be %s" % (groupname, correct_groupname)
+		print "Groupname %s mismatches its permission, its name should be %s" % (groupname, correct_groupname)
 
-			if len(sys.argv) > 0 and sys.argv[0] == 'report':
+		if report_only:
+			continue
+
+		if not match and last:
+			try:
+				group.rename(groupname, correct_groupname)
+				print "Renamed group %s to %s" % (groupname, correct_groupname)
 				continue
+			except GroupExistsError:
+				print "Group %s already exists" % correct_groupname
 
-			if permissions_by_group.index(pbg) == len(permissions_by_group) - 1:
-				try:
-					group.rename(groupname, correct_groupname)
-					print "Renamed group %s to %s" % (groupname, correct_groupname)
-					continue
-				except GroupExistsError:
-					print "Group %s already exists" % correct_groupname
+		try:
+			correct_group = group.Group(correct_groupname)
+		except UnknownGroupError:
+			correct_group = group.add(correct_groupname)
+			print "Created group %s" % correct_groupname
 
+		old_group = group.Group(groupname)
+		members = list(old_group.members())
+		for member in members:
 			try:
-				correct_group = group.Group(correct_groupname)
-			except UnknownGroupError:
-				correct_group = group.add(correct_groupname)
-				print "Created group %s" % correct_groupname
+				correct_group.add_member(user.User(member))
+				print "Added %s to group %s" % (member, correct_groupname)
+			except MemberExistsError:
+				print "User %s is already a member of group %s" % (member, correct_groupname)
+				pass
 
-			old_group = group.Group(groupname)
-			members = list(old_group.members())
-			for member in members:
-				try:
-					correct_group.add_member(user.User(member))
-					print "Added %s to group %s" % (member, correct_groupname)
-				except MemberExistsError:
-					print "User %s is already a member of group %s" % (member, correct_groupname)
-					pass
+		params = [permission_by_group['repository'], permission_by_group['vcs'],
+			permission_by_group['path'], correct_groupname, 'group', permission_by_group['permission']]
+		try:
+			permissions.add(*params)
+			print "Added permission for group %s" % correct_groupname
+		except storage.SQLIntegrityError:
+			permissions.change(*params)
+			print "Changed permission for group %s" % correct_groupname
+		except DoesNotExistError as e:
+			print "Path %s does not exist" % e
 
-			params = [pbg['repository'], pbg['vcs'], pbg['path'], correct_groupname, 'group', pbg['permission']]
-			try:
-				permissions.add(*params)
-				print "Added permission for group %s" % correct_groupname
-			except storage.SQLIntegrityError:
-				permissions.change(*params)
-				print "Changed permission for group %s" % correct_groupname
-			except DoesNotExistError as e:
-				print "Path %s does not exist" % e
+		permissions.remove(permission_by_group['repository'], permission_by_group['vcs'],
+			permission_by_group['path'], groupname, 'group')
+		print "Removed permission for group %s" % groupname
 
-			if permissions_by_group.index(pbg) == len(permissions_by_group) - 1:
-				old_group.remove()
-				print "Removed group %s" % groupname
+		if not match and last:
+			old_group.remove()
+			print "Removed group %s" % groupname
